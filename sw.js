72f2fb6c33ceb182198ca6cfa3a20869780f67cb
Mattias Johansson <mattias@hiberworld.com>
2024-08-26 15:05:41 +0200

    WIP scrub toggle

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
 const CACHE_NAME = 'video-sync-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/main.js',
-  '/videoController.js',
-  '/uiController.js',
-  '/syncController.js',
-  '/audioAnalyzer.js',
-  '/midiController.js',
-  '/utils.js',
-  '/icon-192x192.png',
-  '/icon-512x512.png'
+  'https://kualia.one/syncwave/',
+  'https://kualia.one/syncwave/index.html',
+  'https://kualia.one/syncwave/styles.css',
+  'https://kualia.one/syncwave/main.js',
+  'https://kualia.one/syncwave/videoController.js',
+  'https://kualia.one/syncwave/uiController.js',
+  'https://kualia.one/syncwave/syncController.js',
+  'https://kualia.one/syncwave/audioAnalyzer.js',
+  'https://kualia.one/syncwave/midiController.js',
+  'https://kualia.one/syncwave/utils.js',
+  'https://kualia.one/syncwave/icon-192x192.png',
+  'https://kualia.one/syncwave/icon-512x512.png'
 ];
 
 self.addEventListener('install', function(event) {
